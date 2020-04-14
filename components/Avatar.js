5d93a83a85066cd32f52e879f6ea943b5f8aa959
iYaroslav <samardak.yaroslav@gmail.com>
2020-04-15 00:58:05 +0500

    Change avatar size and position

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,13 +1,12 @@
 import Img from "./Img"
 
-const Avatar = () => <div className='avatar'>
-  <Img
-    alt='Yaroslav Samardak'
-    src={'avatar_kdi0ti.jpg'}
-    flags={['c_thumb', 'g_face']}
-    ratio='1:1'
-    size={160}
-  />
-</div>
+const Avatar = () => <Img
+  className='avatar'
+  alt='Yaroslav Samardak'
+  src={'avatar_kdi0ti.jpg'}
+  flags={['c_thumb', 'g_face']}
+  ratio='1:1'
+  size={100}
+/>
 
 export default Avatar
--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,7 @@ import css from "../styles/_app.scss"
 
 const App = ({Component, pageProps}) => <>
   <Head>
-    <title>CV - Yaroslav Samardak</title>
+    <title>Yaroslav Samardak</title>
     <meta charSet="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;600;700&display=swap" rel="stylesheet"/>
