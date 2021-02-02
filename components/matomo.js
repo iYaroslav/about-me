3b4e5dc24d35c249f5985bb6a7a00b9671356b06
iYaroslav <samardak.yaroslav@gmail.com>
2021-02-02 15:09:02 +0500

    Removed motomo tracker

--- components/matomo.js
+++
@@ -1,9 +0,0 @@
-import MatomoTracker from 'matomo-tracker'
-
-const matomo = new MatomoTracker(1, 'https://leerybituz.matomo.cloud/matomo.php')
-
-matomo.on('error', function(err) {
-  console.log('error tracking request: ', err)
-})
-
-export default matomo
